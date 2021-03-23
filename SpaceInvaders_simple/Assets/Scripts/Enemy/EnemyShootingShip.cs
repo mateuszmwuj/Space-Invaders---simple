d30c52680f4a01809a962484d0f1af63daba1143
ethium_wings <ethium.wings@wp.pl>
2021-03-23 02:13:47 +0100

    [renamed] script

---
+++ SpaceInvaders_simple/Assets/Scripts/Enemy/EnemyShootingShip.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShootingShip : EnemyBasicShip
+{
+    [SerializeField]
+    private EnemyShoot _enemyShoot;
+    public EnemyShoot enemyShoot =>_enemyShoot;
+}
